6d19b7810697f2652459b027fc5126f952f336bc
Pedro Suarez <pedrofrancisco.suarezfeito@verisk.com>
2026-02-02 15:08:26 +0000

    AppointmetApi creation

---
+++ AppointmentsApi/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
